1324869d53f94fdab3df57d4a119e11de7786343
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2025-10-18 19:52:29 +0100

    Add login and secure page

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,40 +5,22 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
     WebDriver driver = new ChromeDriver();
+    protected HomePage homePage;
 
     public void setUp() {
         // open website
         driver.get("https://the-internet.herokuapp.com/");
 
-        // maximize screen
-        // driver.manage().window().maximize();
 
-        // set size
-        // driver.manage().window().setSize(new Dimension(412, 915));
+        homePage = new HomePage(driver);
+        driver.quit();
 
-        // locate an element
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println(links.size());
-
-//        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
-//        inputsLink.click();
-
-        WebElement shiftingContent = driver.findElement(By.linkText("Shifting Content"));
-        shiftingContent.click();
-
-        WebElement menuElement = driver.findElement(By.linkText("Example 1: Menu Element"));
-        menuElement.click();
-
-
-        System.out.println(driver.getTitle());
-
-        // close page
-        // driver.quit();
     }
 
     public static void main(String[] args) {
