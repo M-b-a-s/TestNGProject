f1752432e2df162c2eeedd821a370814cf9144cf
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2025-10-20 05:07:04 +0100

    write tests for dropdown page

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -5,15 +5,23 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage {
     WebDriver driver;
-    By formAuth = By.linkText("Form Authentication");
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
     public LoginPage clickFormAuth() {
-        driver.findElement(formAuth).click();
+        clickLink("Form Authentication");
 
         return new LoginPage(driver);
+    }
+
+    public DropdownPage clickDroopDown(){
+        clickLink("Dropdown");
+        return new DropdownPage(driver);
+    }
+
+    private void clickLink(String linkText) {
+        driver.findElement(By.linkText(linkText)).click();
     }
 }
